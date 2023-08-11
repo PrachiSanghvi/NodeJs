bff2769214a02afe842de7769c305919d9e5bcb8
prachi <prachisanghvi62@gmail.com>
2023-08-11 16:13:44 +0530

    mongo db connnection

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./Models/Todo');
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/test')
+
+app.get('/get', (req, res) => {
+  TodoModel.find().then(result => res.json(result)).
+    catch(err => res.json(err))
+})
+
+app.put('/update/:id', (req, res) => {
+  const {id} = req.params;
+  TodoModel.findByIdAndUpdate({ _id: id},{done:true}).
+  then(result => res.json(result)).
+  catch(err => console.log(err))
+})
+
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+
+})
+
+app.delete('/delete/:id', (req, res) => {
+  const {id} = req.params;
+  TodoModel.findByIdAndDelete({ _id: id}).
+  then(result => res.json(result)).
+  catch(err => console.log(err))
+})
+app.listen(3001, () => {
+  console.log("Server is running!!!nv");
+})
